f671e78f1a1fdbecc9081a642c4094667aed482e
U-BSD\321341 <321341@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-10 09:49:58 -0700

    Learning to push changes

--- gussing.cpp
+++ gussing.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int main() {
   srand(time(NULL));
   int randomNum = rand(); 
-  cout << randomNum;
+  // cout << randomNum;
+  int playerNum = cin >> "Guess a number! ";
+  int guessCount = 0;
+  while(playerNum != randomNum) {
+    //do something?
+  }
   return 0;
 }
