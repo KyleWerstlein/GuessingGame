d6021188a1deecee8486f19fdf5ed37f11bed47e
U-BSD\321341 <321341@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-10 10:38:41 -0700

    guessing now exists

--- gussing.cpp
+++ gussing.cpp
@@ -5,12 +5,28 @@ using namespace std;
 
 int main() {
   srand(time(NULL));
-  int randomNum = rand(); 
-  // cout << randomNum;
-  int playerNum = cin >> "Guess a number! ";
-  int guessCount = 0;
-  while(playerNum != randomNum) {
-    //do something?
+  int randomNum = rand() % 101; // Help from Cal limiting random number range. 
+  cout << "Random number" << randomNum << endl;
+  int playerNum = 0;
+  cout << "Guess a number!" << endl;
+  cin >> playerNum;
+  int guessCount = 1;
+  while((playerNum != randomNum) && (guessCount != 100)) {
+    int var2;
+    if(playerNum > randomNum) {
+      cout << "Too High! " << endl;
+      cout << "Guess a number! " << endl;
+      cin >> var2;
+      playerNum = var2;
+      guessCount++;
+    }
+    else if(playerNum < randomNum) {
+      cout << "Too Low! " << endl;
+      cout << "Guess a number!" << endl;
+      cin >> var2;
+      playerNum = var2;
+      guessCount++;
+    }
   }
   return 0;
 }
